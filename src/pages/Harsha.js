401d9bae4f586f0e50470059151274eb437cba75
Harsha Kalavagunta <134469921+Harsha-Kal@users.noreply.github.com>
2024-11-17 23:05:25 -0700

    Add Personal Page

---
+++ src/pages/Harsha.js
@@ -0,0 +1,121 @@
+export default function examplePage() {
+    return (
+      <div className='container'>
+        <title>Website</title>
+        <link rel='icon' href='/favicon.ico' />
+
+        <main>
+          <div className='underline'>
+            <h1 className='title'>Hello, I am Harsha. </h1>
+          </div>
+
+          <p className='description'>
+        Tesla, Inc. is an American electric vehicle (EV) and clean energy company founded in 2003
+        by engineers Martin Eberhard and Marc Tarpenning, with Elon Musk joining shortly after as
+        an investor and later CEO. Tesla has become a global leader in electric vehicles, pioneering
+        the mass production of EVs with models such as the Model S, Model 3, Model X, and Model Y.
+        The company's mission is to accelerate the world's transition to sustainable energy,
+        not only through electric vehicles but also by advancing solar power technology and energy
+        storage solutions. Tesla's innovations extend beyond cars, with notable achievements in
+        autonomous driving, battery technology, and Supercharger infrastructure. Its impact on
+        the automotive and energy industries has redefined the future of transportation and renewable energy.
+          </p>
+
+          <a className='button' href='/'>
+            <p> Back to Home Page</p>
+          </a>
+        </main>
+
+        <style jsx>{`
+          .container {
+            min-height: 100vh;
+            padding: 0 0.5rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            max-width: 60rem;
+            margin: auto;
+          }
+
+          main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .title {
+            margin: 0;
+            line-height: 1.15;
+            font-size: 4rem;
+          }
+
+          .underline {
+            /* border-bottom: solid black; */
+            margin-bottom: 1.5rem;
+          }
+
+          .description {
+            line-height: 1.5;
+            font-size: 1.5rem;
+          }
+
+          .pic {
+            height: 450px;
+            width: 450px;
+          }
+
+          .button {
+            /* margin: .5rem; */
+            flex-basis: 45%;
+            padding: 0.5rem;
+            text-align: left;
+            color: inherit;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+
+          .button:hover,
+          .button:focus,
+          .button:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+
+          .button p {
+            font-size: 1rem;
+            font-weight: 600;
+          }
+
+          code {
+            background: #fafafa;
+            border-radius: 5px;
+            padding: 0.75rem;
+            font-size: 1.1rem;
+            font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+              DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          }
+        `}</style>
+
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+      </div>
+    );
+}
